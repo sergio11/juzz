6c2c0fc0bf8a8dd511d56a7ab5a90925d154f8a6
Sergio <sss4esob@gmail.com>
2016-02-09 09:58:39 +0100

    Paso de DoctrineFixturesBundle al entorno de desarrollo y test

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new DCS\OpauthBundle\DCSOpauthBundle(),
             new juzz\CanalesBundle\juzzCanalesBundle(),
             new juzz\ProgramasBundle\juzzProgramasBundle(),
@@ -43,6 +42,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
